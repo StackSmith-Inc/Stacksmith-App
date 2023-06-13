c3bf482d264b86a910f13e05ca8cdc7883608954
Camina Drummer <drummer.camina@pm.me>
2023-06-13 17:17:38 -0400

    Update config files, tailwind still needs update

---
+++ client/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <>
+      <p className="text-lg text-red-500">Hello!</p>
+    </>
+  )
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./client/**/*.{js,jsx}', './client/index.html'],
+  theme: {
+      extend: {
+          colors: {
+          },
+      },
+  },
+  plugins: []
+};
