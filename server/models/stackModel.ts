d2b7c43d18e55c45f15f9e5eece5e239a43fd525
johncamilomcconnell <johncmcconnell@gmail.com>
2023-06-13 17:03:47 -0400

    backend build out

---
+++ server/models/stackModel.ts
@@ -0,0 +1,17 @@
+const { Pool } = require('pg')
+
+const URI = 'postgres://cuyzknub:uWRJI3AaZc2U79x4HEhBaIwNts4oL-_F@fanny.db.elephantsql.com/cuyzknub'
+
+const pool = new Pool ({
+    connectionString: URI
+})
+
+//add schema here
+
+module.exports = {
+    query: (text: String, params: String, callback: any) => {
+        console.log("executed query", text);
+        return pool.query(text, params, callback)
+    }
+}
+
---
+++ server/server.ts
@@ -0,0 +1,32 @@
+const pg = require('pg');
+const express = require('express');
+const app = express();
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+//add routes here
+
+
+app.use('*', (req: Request, res: Response) => {
+    res.sendStatus(404);
+  });
+app.use((req: Request, res: Response, err: Object, next: NextFunction) => {
+    const defaultErr = {
+      log: 'Express error handler hiiiiii caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error global occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+
+
+app.listen(PORT, () => {
+  console.log('listening on port: ', PORT)
+});
+
