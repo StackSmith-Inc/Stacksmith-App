9776955c638c58d8bb698f8663e882b02c8a35c5
Camina Drummer <drummer.camina@pm.me>
2023-06-13 15:53:58 -0400

    Start project

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env'], '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    compress: true,
+    historyApiFallback: true,
+    port: 8080,
+    proxy: {
+      '/api/**': 'http://localhost:3000',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
