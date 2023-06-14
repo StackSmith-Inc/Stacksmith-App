a6f6bc58e8049a168bdbdd85793ec644ee2d5e9a
Camina Drummer <drummer.camina@pm.me>
2023-06-14 17:02:14 -0400

    Update path for stackSourcesSlice in App

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { load } from '../redux/stackOptionsSlice';
+import { load } from '../redux/stackSourcesSlice';
 import Options from './Options';
 import Nav from './Nav';
 
