71c4cc22804a75fdf73224b47d7ebbcd6c7ba805
Camina Drummer <drummer.camina@pm.me>
2023-06-15 10:07:00 -0400

    Separate options and picked

--- client/components/techBox.tsx
+++ client/components/techBox.tsx
@@ -1,41 +1,29 @@
-// import React from 'react'
-// import { RootState } from '../redux/store';
-// import { useSelector, useDispatch } from 'react-redux'
+import React from 'react'
+import { RootState } from '../redux/store';
+import { useSelector, useDispatch } from 'react-redux'
 
+interface techBoxProps {
+  techObj: techObj,
+  handleClick: (e: any) => void
+}
 
-// interface techObj {
-//     techname: string,
-//     techtype: string,
-//     techdescription: string,
-//     img: string
-//   }
+interface techObj {
+    techname: string,
+    techtype: string,
+    techdescription: string,
+    img: string
+  }
 
-// const techBox: React.FC = (props) => {
-//   const dispatch = useDispatch();
-//   const allTechs = useSelector((state: RootState) => state.stackSources.allTechs);
+const techBox: React.FC = (props: techBoxProps) => {
+  const dispatch = useDispatch();    
+  return (
+    <>
+      <div className="tooltip" data-tip={props.techObj.techdescription}>
+        <p>{props.techObj.techname}</p>
+        <img onClick={(e) => dispatch(props.handleClick(e))} className='btn' src={props.techObj.img}/>
+      </div>
+    </>
+  )
+}
 
-
-//   const textBox = allTechs.map((el: techObj): any => {
-//     console.log(el);
-//     return (
-//     <div key={el.techname}>
-
-//     <img onClick={() => dispatch(addTech(el))} className='btn' src={el.img}>
-//     </img>
-
-//     <div>
-//         <p>{el.techname}</p>
-//     </div>
-//     </div>
-//     )
-    
-//     });
-    
-//   return (
-//     <>
-//     {textBox}
-//     </>
-//   )
-// }
-
-// export default techBox;
+export default techBox;
